9e911a77a1b27df22d5403ab6ea7ccc98bedc294
Lionel Desorme <lionel.desorme@free.fr>
2019-06-01 11:06:06 +0200

    Initial commit

---
+++ TestDnsFail/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDnsFail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            try
+            {
+                var connection = new TcpClient("toto", 123);
+                //connection.Connect();
+                Console.WriteLine("Connected");
+                }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"Sock exception: {e.Message} => ec:{e.SocketErrorCode}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+          //  throw new Exception("Error");
+        }
+    }
+}
